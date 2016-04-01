62b14cc93800418521e0c1ce1319a4f31c6e9359
Stanislal <icru2@hotmail.com>
2016-04-01 16:28:43 -0300

    ControlePersonagem2
    IREI COMMIATAR NO NOVO BRUZIN WORD? isso n da probleminha? pq é 2
    arquivinhos entao o resto fica identico né?

---
+++ Assets/Script/Personagem/ControlePersonagem2.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+[NetworkSettings(channel=1,sendInterval=0f)]
+
+public class ControlePersonagem2 : NetworkBehaviour {
+
+	private float velocidade = 5;
+	[SyncVar] Vector2 pos = new Vector2(0,0);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+		
+	//Movimentação!!
+	void Movimentacao() {
+			// Ve se você tem autoridade sobre o personagem para movelo
+		if (hasAuthority == true) {
+			//Anda para a direita
+			if (Input.GetAxisRaw ("Horizontal") > 0) {
+				transform.Translate (Vector2.right * velocidade * Time.deltaTime);
+				transform.eulerAngles = new Vector2 (0, 0);
+				//Pega a Posição do personagem e envia para o server (host)
+				CmdSetPos (transform.position);
+			}
+
+			//Anda para a esquerda
+			if (Input.GetAxisRaw ("Horizontal") < 0) {
+				transform.Translate (Vector2.right * velocidade * Time.deltaTime);
+				transform.eulerAngles = new Vector2 (0, 180);
+				//Pega a Posição do personagem e envia para o server (host)
+				CmdSetPos (transform.position);
+			}
+		}else {
+			// Não sei!
+				gameObject.transform.position = new Vector3 (pos.x, pos.y, gameObject.transform.position.z);
+			}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			Movimentacao();
+	}
+
+	[Command] void CmdSetPos(Vector2 position){
+		pos = position;
+	}
+}
