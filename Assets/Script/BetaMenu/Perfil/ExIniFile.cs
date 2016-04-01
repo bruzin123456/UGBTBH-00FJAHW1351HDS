ba94fae68620b232a75e2e54082bc4e0df0a6e81
Stanislal <icru2@hotmail.com>
2016-04-01 10:45:35 -0300

    Criação do Conceito .Ini
    Criação do script Ini para salvar as informações do usuario a cada vez
    que ele inciializar o jogo carregar a mesma. (ainda n implementado
    somente testei e criei , ainda n sincronizei com o alterar nome pois não
    sei como a gente vai fazer essa padronização.

---
+++ Assets/Script/BetaMenu/Perfil/ExIniFile.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ExIniFile : MonoBehaviour {
+
+	public string Key; //Chave
+	public string Section; // Sessão
+	public static string SomeValue; // valor da chave
+
+
+	// adicinar o endreço para ele pegar do executavel, (ainda n implementado)
+	void Start () {
+		// Escreve no arquivo.ini section, key e valor, no endereço.
+		IniFile.WriteValue(Section, Key, SomeValue, "C:\\Users\\Stanislal\\Documents\\GitHub\\UGBTBH-00FJAHW1351HDS\\config.ini");
+
+
+		//Provavelmente separar o Start em 2 Gravar e Carregar!
+		//Busca no inifile.cs a procedure que busca o arquivo com sua sessão e sua chave
+		string value = IniFile.ReadValue(Section, Key, "C:\\Users\\Stanislal\\Documents\\GitHub\\UGBTBH-00FJAHW1351HDS\\config.ini");
+
+		Console.WriteLine(value);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
