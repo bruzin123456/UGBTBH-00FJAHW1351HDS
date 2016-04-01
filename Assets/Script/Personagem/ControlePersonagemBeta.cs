d7879bd8f637636fed77c68f1d94ba7ff6b3adb7
Bruno Reis Galindo <brugalindo2@hotmail.com>
2016-04-01 15:16:42 -0300

    Ajustes

--- Assets/Script/Personagem/ControlePersonagemBeta.cs
+++ Assets/Script/Personagem/ControlePersonagemBeta.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.Networking;
-
+[NetworkSettings(channel=1,sendInterval=0f)]
 public class ControlePersonagemBeta : NetworkBehaviour {
 	private float vel = 5;
 	[SyncVar] Vector2 pos = new Vector2(0,0);
@@ -13,12 +13,14 @@ public class ControlePersonagemBeta : NetworkBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		gameObject.transform.position = new Vector3 (pos.x, pos.y, gameObject.transform.position.z);
-		if(hasAuthority == true){
+		if (hasAuthority == true) {
 			if (Input.GetAxis ("Horizontal") != 0 || Input.GetAxis ("Vertical") != 0) {
-				CmdSetPos(new Vector2(transform.position.x + vel*Time.deltaTime*Input.GetAxis("Horizontal"), transform.position.y + vel*Time.deltaTime*Input.GetAxis("Vertical")));
+				transform.position = new Vector3 (transform.position.x + vel * Time.deltaTime * Input.GetAxis ("Horizontal"), transform.position.y + vel * Time.deltaTime * Input.GetAxis ("Vertical"), 0);
+				CmdSetPos (transform.position);
+			}
+		} else {
+			gameObject.transform.position = new Vector3 (pos.x, pos.y, gameObject.transform.position.z);
 		}
-					}
 	}
 
 
