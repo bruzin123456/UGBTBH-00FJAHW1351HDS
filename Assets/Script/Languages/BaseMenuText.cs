0234acba5833249001ac4f18f59b8feb91c162d7
Bruno Reis Galindo <brugalindo2@hotmail.com>
2016-04-01 22:12:15 -0300

    Add SpawnPoint \\ configurações de linguagem

---
+++ Assets/Script/Languages/BaseMenuText.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseMenuText {
+	public string profile;
+	public string options;
+	public string quitgame;
+	public string screenResolution;
+	public string screenMode;
+	public string apply;
+	public string name;
+	public string timePlayed;
+	public string Progress;
+	public string change;
+	public string returnToMenu;
+	public string join;
+	public string host;
+	public string startGame;
+	public string disconnect;
+	public string window;
+	public string fullScreen;
+	public string save;
+	public string confirm;
+	public string cancel;
+	public string returnn;
+
+	}
+
+public class PortugueseMenuText :  BaseMenuText{
+	public PortugueseMenuText(){
+		profile = "Perfil";
+		options = "Opções";
+		quitgame = "Sair Do jogo";
+		screenResolution = "Resolução da Tela:";
+		screenMode = "Modo de Tela:";
+		apply = "Aplicar";
+		name = "Nome:";
+		timePlayed = "Tempo Jogado:";
+		Progress = "Progresso:";
+		change = "Trocar";
+		returnToMenu = "Voltar Menu";
+		join = "Juntar-se";
+		host = "Hostear";
+		startGame = "Iniciar Jogo";
+		disconnect = "Desconectar";
+		window = "Janela";
+		fullScreen = "Tela Cheia";
+		save = "Salvar";
+		confirm = "Confirmar";
+		cancel = "Cancelar";
+		returnn = "Voltar";
+	}
+
+
+}
+
--- Assets/Script/Personagem/ControlePersonagemBeta.cs
+++ Assets/Script/Personagem/ControlePersonagemBeta.cs
@@ -1,30 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.Networking;
-[NetworkSettings(channel=1,sendInterval=0f)]
+[NetworkSettings(channel=0,sendInterval=0f)]
 public class ControlePersonagemBeta : NetworkBehaviour {
 	private float vel = 5;
-	[SyncVar] Vector2 pos = new Vector2(0,0);
+	[SyncVar] Vector2 pos;
+	[SyncVar] bool Paused = true;
 
-	// Use this for initialization
+
 	void Start () {
-		
+		if (isServer) 
+			SpawnPoint ();	
 	}
 	
-	// Update is called once per frame
+
 	void Update () {
-		if (hasAuthority == true) {
-			if (Input.GetAxis ("Horizontal") != 0 || Input.GetAxis ("Vertical") != 0) {
-				transform.position = new Vector3 (transform.position.x + vel * Time.deltaTime * Input.GetAxis ("Horizontal"), transform.position.y + vel * Time.deltaTime * Input.GetAxis ("Vertical"), 0);
-				CmdSetPos (transform.position);
+		if (Paused == false) {
+			if (hasAuthority == true) {
+				if (Input.GetAxis ("Horizontal") != 0 || Input.GetAxis ("Vertical") != 0) {
+					transform.position = new Vector3 (transform.position.x + vel * Time.deltaTime * Input.GetAxis ("Horizontal"), transform.position.y + vel * Time.deltaTime * Input.GetAxis ("Vertical"), 0);
+					CmdSetPos (transform.position);
+				}
+			} else {
+				gameObject.transform.position = new Vector3 (pos.x, pos.y, gameObject.transform.position.z);
 			}
-		} else {
-			gameObject.transform.position = new Vector3 (pos.x, pos.y, gameObject.transform.position.z);
 		}
+	}
+
+
+	void SpawnPoint(){
+		Vector2 posicao;
+		if (gameObject.name == "Player1") {
+			posicao = GameObject.Find ("SpawnP1").transform.position;		
+		} else {
+			posicao = GameObject.Find ("SpawnP2").transform.position;
+		}
+		pos = posicao;
+		RpcSpawnPos (posicao);
+		Paused = false;
 	}
 
 
 	[Command] void CmdSetPos(Vector2 position){
 		pos = position;
+	}
+
+
+	[ClientRpc] void RpcSpawnPos(Vector2 position){
+		gameObject.transform.position = new Vector3 (position.x, position.y, gameObject.transform.position.z);
 	}
 }
